4b8155fdc6ad98841c01f6745a970899f0329b47
Adam Zygadlewicz <adam@zygadlewicz.com>
2021-10-01 23:39:02 +0200

    added api cache

--- settings/index.js
+++ settings/index.js
@@ -93,7 +93,7 @@ const settings =  {
             'admin': '1751bfe48d5ad21fd9d'
         },
         lang_api_endpoint : 'https://api.eventjuicer.com/proxy?url=https%3A%2F%2Flocalise.biz%2Fapi%2Fexport%2Fall.json%3Fformat%3Dmulti%26pretty%26key%3DSHiwxgKaPMx_KThQH2zcdzwiKEMzuNBm',    
-        api : "https://api.eventjuicer.com/v1/public/hosts/targiehandlu.pl",
+        api : "https://api.eventjuicer.workers.dev?url=https%3A%2F%2Fapi.eventjuicer.com%2Fv1%2Fpublic%2Fhosts%2Ftargiehandlu.pl",
         // api : "http://eventjuicer-api.test/v1/public/hosts/targiehandlu.pl",
         post_api : "https://api.eventjuicer.com/v1/public/hosts/targiehandlu.pl/register",
         service_api : "https://api.eventjuicer.com/v1/services",
