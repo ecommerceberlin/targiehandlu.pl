c763d3efede4f0b1aa68003b5dc3f9034225b68a
Adam Zygadlewicz <adam@zygadlewicz.com>
2021-10-07 02:32:07 +0200

    enabled all stages

--- settings/index.js
+++ settings/index.js
@@ -178,10 +178,10 @@ const settings =  {
             '16:05': 'presentation'
           }, 
           venues : {
-            // A: { company_id: 0 },
+            A: { company_id: 0 },
             B: { company_id: 0 },
             C: { company_id: 0 },
-            // D: { company_id: 0 },
+            D: { company_id: 0 },
             E: { company_id: 0 },
             F: { company_id: 0 },
           },
