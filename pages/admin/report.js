3f238c9001d83153c0868a354780a38f5188cee2
Adam Zygadlewicz <adam@zygadlewicz.com>
2021-10-16 15:20:19 +0200

    aggregated catering $30$

--- pages/admin/report.js
+++ pages/admin/report.js
@@ -6,13 +6,39 @@ import {
     Exhibitor,
     reduxWrapper,
     configure,
-    useDatasource
+    useDatasource,
+    Alert
   } from 'eventjuicer-site-components';
   
   import {useRouter} from 'next/router'  
   
   const settings = require('../../settings').default;
 
+  const reducer = (acc, currentValue) => acc + currentValue.quantity;
+
+  const howManyBooths = (purchases) => purchases.filter(item => item.role=="exhibitor").length
+  const howManyCatering = (purchases) => purchases.filter(item => item.id == 1776).reduce(reducer, 0)
+  const howManyParking = (purchases) => purchases.filter(item => item.id == 1780).reduce(reducer, 0)
+
+  const ExhibitorAlert = ({reps, purchases}) => {
+
+    const booths = howManyBooths(purchases)
+    const catering = howManyCatering(purchases)
+    const parking = howManyParking(purchases)
+
+    const cateringReal = () => {
+      if(reps > booths * 4){
+        return (booths * 4) + catering
+      }else{
+        return reps + catering
+      }
+    }
+
+    const parkingReal = () => booths * 1 + parking
+
+    return <Alert content={<>Catering: <strong>{cateringReal()}</strong> {` `}
+    Parking: <strong>{parkingReal()}</strong></>} />
+  }
 
   const PageAdminReport = () => {
   
@@ -20,9 +46,7 @@ import {
       sort: "profile.booth"
     }})
     
-    const {query, asPath} = useRouter();
-    
-    console.log(query, asPath)
+    const {query} = useRouter();
     
     //parse params!
     
@@ -54,7 +78,17 @@ import {
   
 
   
-   return (<Wrapper>{data.map(exhibitor => (<Exhibitor key={exhibitor.id} {...exhibitor} roles={["presenter","service_internal","service_external"]} />))}</Wrapper>)
+   return (<Wrapper>{data.map(exhibitor => {
+
+
+    return (<Exhibitor 
+      key={exhibitor.id} 
+      {...exhibitor} 
+      roles={["presenter","service_internal","service_external"]}
+      alert={<ExhibitorAlert key={exhibitor.id}  {...exhibitor} />}
+      />)
+
+   })}</Wrapper>)
   
   }
   
