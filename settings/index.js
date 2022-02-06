8999ad068f4605be8180fa9e6568337cf7b05dac
Adam Zygadlewicz <adam@zygadlewicz.com>
2022-02-06 22:11:41 +0100

    updated date

--- settings/index.js
+++ settings/index.js
@@ -735,7 +735,7 @@ const settings =  {
         items : [
             {date: "2021-12-16", name: "submissions", icon:  "NoteAdd", dotColor: 'primary', active: true },
             {date: "2022-02-06", name: "qualification", icon:  "Assessment" },
-            {date: "2022-02-07", name: "public-voting", icon:  "Public" },
+            {date: "2022-02-09", name: "public-voting", icon:  "Public" },
             // {date: "2020-10-26", name: "jury-voting", icon:  "HowToVote" },
             {date: "2022-02-25", name: "results", icon:  "Mic", active: false },
             {date: "2022-03-04", name: "formal", icon:  "Settings", active: false }
