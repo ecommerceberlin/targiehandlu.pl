bb33f77ec856f754294af75600a3bd6e08cdddee
Adam Zygadlewicz <adam@zygadlewicz.com>
2022-02-07 00:01:27 +0100

    build for cfp voting $20$

--- pages/vote/index.js
+++ pages/vote/index.js
@@ -1,52 +1,51 @@
 import {
-    connect,
-    MyHead as Head,
-   // WidgetSalesMap,
-    WidgetVoteStatus,
-    // WidgetRoleButtons,
-    LayoutMain as Layout,
-    MyTypography as Typography,
-    Markdown,
-    reduxWrapper,
-    configure,
-    Centered
-  } from 'eventjuicer-site-components';
-  
+  connect,
+  MyHead as Head,
+ // WidgetSalesMap,
+  WidgetVoteStatus,
+  // WidgetRoleButtons,
+  LayoutMain as Layout,
+  MyTypography as Typography,
+  Markdown,
+  reduxWrapper,
+  configure,
+  Centered
+} from 'eventjuicer-site-components';
+
 
 import VotingCategories from '../../components/VotingCategories'
 
 const settings = require('../../settings').default;
-  
-  const PageVote  = () => (
-  
-    <VotingCategories intro={
-      
-      <Centered>
-          <div style={{ maxWidth: 600, marginLeft: "auto", marginRight: "auto", marginBottom: 50}}>
-      <WidgetVoteStatus max_votes={6} />
-        <Typography template="benefitsText">
-          <Markdown label="callforpapers.voting.general-rules.description" />
-        </Typography>
-        </div>
-        </Centered>
-     
-    } />) 
-  
+
+const PageVote  = () => (
+
+  <VotingCategories intro={
+    
+    <Centered>
+        <div style={{ maxWidth: 600, marginLeft: "auto", marginRight: "auto", marginBottom: 50}}>
+    <WidgetVoteStatus max_votes={6} />
+      <Typography template="benefitsText">
+        <Markdown label="callforpapers.voting.general-rules.description" />
+      </Typography>
+      </div>
+      </Centered>
    
-  
-  export const getStaticProps = reduxWrapper.getStaticProps(async ( props ) => {
-  
-    await configure(props, {
-      settings : settings,
-      preload : ["callforpapers"]
-    })
-  
-    return {
-      props : {},
-      revalidate: 10
-    }
-  
+  } />) 
+
+ 
+
+export const getStaticProps = reduxWrapper.getStaticProps(async ( props ) => {
+
+  await configure(props, {
+    settings : settings,
+    preload : ["callforpapers"]
   })
-  
-  export default connect()(PageVote);
-  
+
+  return {
+    props : {},
+    revalidate: 30
+  }
+
+})
+
+export default connect()(PageVote);
