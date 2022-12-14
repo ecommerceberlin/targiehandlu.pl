e3ec3566e3d568de4f7b4ea432c7f61fc30a3fa5
Adam Zygadlewicz <adam@zygadlewicz.com>
2022-12-14 14:56:46 +0100

    Update index.js

--- settings/index.js
+++ settings/index.js
@@ -861,7 +861,7 @@ const settings =  {
         secondaryStyle: null,
       //  iconStyle: "heroIcon",
         links : [
-            {label: "exhibitors.agreement.title", href : "/legal-2022-11-09"},
+            {label: "exhibitors.agreement.title", href : "/legal"},
             {label: "visitors.legal.title", href : "/legal/visitors"},
             {label: "visitors.legal-gdpr.title", href : "/legal/visitors-data"},
         ]
