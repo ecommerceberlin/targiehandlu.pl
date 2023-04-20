db7836817393332f318b7d1fbbe538c816ea4737
Adam Zygadlewicz <adam@zygadlewicz.com>
2023-04-20 09:58:12 +0200

    fixed legal footer link, fixed transaction t&c source file
    resolves https://github.com/ecommerceberlin/targiehandlu.pl/issues/126

--- settings/index.js
+++ settings/index.js
@@ -882,7 +882,7 @@ const settings =  {
         secondaryStyle: null,
       //  iconStyle: "heroIcon",
         links : [
-            {label: "exhibitors.agreement.title", href : "/legal"},
+            {label: "exhibitors.agreement.title", href : "/legal-2023-04-18"},
             {label: "visitors.legal.title", href : "/legal/visitors"},
             {label: "visitors.legal-gdpr.title", href : "/legal/visitors-data"},
         ]
