f836d8a9dd8324a7a40cc5794f07e335b4f2b779
Adam Zygadlewicz <adam@zygadlewicz.com>
2025-11-04 12:33:07 +0100

    remove proxy from localise

--- settings/index.js
+++ settings/index.js
@@ -239,7 +239,7 @@ const settings =  {
         passwords : {
             'admin': '1751bfe48d5ad21fd9d'
         },
-        lang_api_endpoint : 'https://api.eventjuicer.com/proxy?url=https%3A%2F%2Flocalise.biz%2Fapi%2Fexport%2Fall.json%3Fformat%3Dmulti%26pretty%26key%3DSHiwxgKaPMx_KThQH2zcdzwiKEMzuNBm',    
+        lang_api_endpoint : 'https://localise.biz/api/export/all.json?format=multi&key=SHiwxgKaPMx_KThQH2zcdzwiKEMzuNBm',    
         api : "https://api.eventjuicer.com/v1/public/hosts/targiehandlu.pl",
         // proxy: "https://api.eventjuicer.workers.dev?url=",
         // api : "http://eventjuicer-api.test/v1/public/hosts/targiehandlu.pl",
